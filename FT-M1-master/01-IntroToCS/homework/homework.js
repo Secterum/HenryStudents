dab6445f4b673f39016fdad522e472dad917cf8c
Secterum <andresdelfabro99@gmail.com>
2022-08-02 15:59:54 -0300

    tarea

---
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  // tu codigo aca
+/*Dado un número entero (o número de JavaScript),
+seguimos dividiendo el número por dos y capturando su resto hasta que el número se convierte en menos de 2.
+Por ejemplo, si tenemos un número 25,
+sigue dividiendo 25 por 2 hasta que obtengamos el cociente menor que 2.*/
+/*let resul = 0
+let numReverse = num.split('').reverse().join('')
+for (let i = 0; i < numReverse.length; i++) {                   // ACA SE RESOLVIO SOLO CON LOGICA PROPIA.
+    resul += +num[i] * 2 ** (num.length - 1 - i);
+}
+return resul;
+}*/
+//LUEGO COMENCE A BUSCAR FORMAS MAS ACOTADAS. Y ENCONTRE DIFERENTES POSIBILIDADES DEL parseInt entre ellas de resolver el ejercicio.
+return parseInt(num, 2)
+}
+
+
+function DecimalABinario(num) {
+  // tu codigo aca
+  return num.toString(2)
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
